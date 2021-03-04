8868eaf8536bc04a618ae9badf950d7c51e3673f
ElGerarder <gerardo.villanueva.granados@gmail.com>
2021-03-04 11:11:14 -0500

    Cambio de Archivos
    Cambio de Archivos completos.

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/CIM4/';
+	public $baseURL = 'http://localhost:8080/CPEI/';
 
 	/**
 	 * --------------------------------------------------------------------------
---
+++ app/Views/Layout/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CPEI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="public/Styles/Base/reset.css" rel="stylesheet"> 
+    <link href="public/Styles/Pages/index.css" rel="stylesheet">
+    <link href="public/Styles/Layout/header.css" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?php echo base_url(); ?>">
+                <img src="public/img/logo-3.png" alt="LogoCPEI">
+            </a>
+            <div class="navbar-nav ml-lg-auto">
+                <a class="nav-link" href="<?php echo base_url(); ?>">Inicio</a>
+                <a class="nav-link" href="#">Nosotros</a>
+                <a class="nav-link" href="#">Eventos</a>
+                <a class="nav-link" href="#">CPEI Publicaciones</a>
+            </div>
+            </div>
+        </div>
+    </nav>
